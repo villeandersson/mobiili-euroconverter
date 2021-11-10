439123f2dd8db76b6534b2b941fc722e11c543b5
Ville Andersson <winstoni@windowslive.com>
2021-11-10 21:55:21 +0200

    euroconverter

--- App.js
+++ App.js
@@ -1,12 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput, Image } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import data from "./sample_data.json";
 
 export default function App() {
+  const [amount, setAmount] = useState(0);
+  const [conversion, setConversion] = useState(0);
+  const [selected, setSelected] = useState("");
+  const [valuutat, setValuutat] = useState({
+    key: Object.keys(data.rates),
+    value: Object.values(data.rates),
+  });
+  const [rates, setRates] = useState(0);
+
+  const Convert = () => {
+    setConversion(amount / rates);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        style={{ width: 200, height: 200 }}
+        source={require("./euro.png")}
+      />
+      <Text>{conversion}</Text>
+      <View style={styles.input}>
+        <TextInput
+          style={{ fontSize: 18, height: 50, width: 50 }}
+          placeholder="0"
+          keyboardType="numeric"
+          onChangeText={(maara) => setAmount(maara)}
+        />
+        <Picker
+          style={{ height: 50, width: 130 }}
+          selectedValue={selected}
+          onValueChange={(itemValue, itemIndex) => {
+            if (itemIndex != 0) {
+              setSelected(itemValue);
+              setRates(valuutat.value[itemIndex]);
+            }
+          }}
+        >
+          {valuutat.key.map((key) => (
+            <Picker.Item label={key} value={key} key={key} />
+          ))}
+        </Picker>
+      </View>
+      <Text>{"   "}</Text>
+      <Button title="Convert" onPress={Convert} />
     </View>
   );
 }
@@ -14,8 +56,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    flexDirection: "row",
   },
 });
